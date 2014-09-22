89c4d0159582a2adf4db9140c95a200c7e999e62
Juston Cheney <juston.cheney@gmail.com>
2014-09-22 08:07:31 -0600

    Added animation to front page

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Welcome to OpenShift</title>
   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-  
+
 <style>
 body {
 	margin:0;
@@ -13,6 +13,45 @@ body {
 	background-color:rgba(0,0,0,0.9);
 	color:white;
 	font-family: 'Open Sans', sans-serif;
+}
+#the_page {
+	height:100%;
+	width:100%;
+	position: absolute;
+	background-color:white;
+	margin-left:-20px;
+	-webkit-animation: display_page 2s ease;
+	animation: display_page 2s ease;
+}
+@-webkit-keyframes display_page {
+	0% { height:20px; width:0; }
+	50% { height:20px; width:100%;}
+	100% {}
+}
+@keyframes display_page {
+	0% { height:20px; width:0; }
+	50% { height:20px; width:100%;}
+	100% {}
+}
+#front_page_text {
+	margin-left:40px;
+	color:rgba(0,0,0,0.9);
+	-webkit-animation: front_text_slide 3s ease;
+	animation: front_text_slide 3s ease;
+}
+@-webkit-keyframes front_text_slide {
+	0% { margin-left:-100%; }
+	60% { margin-left:-100%; }
+	88% { margin-left:50px; }
+	94% { margin-left:34px; }
+	100% {}
+}
+@keyframes front_text_slide {
+	0% { margin-left:-100%; }
+	60% { margin-left:-100%; }
+	88% { margin-left:50px; }
+	94% { margin-left:34px; }
+	100% {}
 }
 
 </style>
@@ -22,6 +61,11 @@ body {
 
 <section class='container'>
     <h1>Hello World</h1>
+    <div id="the_page">
+    	<div id="front_page_text">
+    		<p>Welcome to App-Juston.</p>
+    	</div>
+    </div>
 </section>
 
 </body>
