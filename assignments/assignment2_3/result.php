b081b4be99e2cae8553ab35d05889be73e62b858
Juston Cheney <juston.cheney@gmail.com>
2014-10-04 21:59:34 -0600

    Prepped for the web

--- assignments/assignment2_3/result.php
+++ assignments/assignment2_3/result.php
@@ -5,8 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>PHP Blitz</title>
   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-  <link href="/sandboxing/css/styles.css" rel="stylesheet" type="text/css">
-  <script src="/sandboxing/js/header.js" type="text/javascript"></script>
+  <link href="/css/styles.css" rel="stylesheet" type="text/css">
+  <script src="/js/header.js" type="text/javascript"></script>
 
 </head>
 <body class="front-page">
