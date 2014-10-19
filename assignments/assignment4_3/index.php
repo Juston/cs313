94bb70d15336e2286b80c169f3f591bbd092a7a3
Juston Cheney <juston.cheney@gmail.com>
2014-10-18 20:37:35 -0600

    Commented out for each

--- assignments/assignment4_3/index.php
+++ assignments/assignment4_3/index.php
@@ -4,13 +4,13 @@
 //  include library.php';
 //  include 'model.php';
 
-if($_POST['action'] = "Manual Search" || $_POST['action'] = "Auto Search") {
-  var $fastfood_options = fastfoodOptions();
+// if($_POST['action'] = "Manual Search" || $_POST['action'] = "Auto Search") {
+//   var $fastfood_options = fastfoodOptions();
 
-  var $option_outputs = "";
-  foreach($fastfood_options as $option) {
-    $option_outputs .= '<p>Name: ' . $option['fastfoodName'] . '<br>' . $option['fastfoodLocation'] . '</p>;
-  }
-}
+//   var $option_outputs = "";
+//   foreach($fastfood_options as $option) {
+//     $option_outputs .= '<p>Name: ' . $option['fastfoodName'] . '<br>' . $option['fastfoodLocation'] . '</p>;
+//   }
+// }
 
   include 'view.php';
