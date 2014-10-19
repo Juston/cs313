9fe00992bde3dea473e5fcac1fa50175acb3f1f5
Juston Cheney <juston.cheney@gmail.com>
2014-10-18 20:48:11 -0600

    Not much

--- assignments/assignment4_3/index.php
+++ assignments/assignment4_3/index.php
@@ -5,20 +5,21 @@
 //  include 'model.php';
 
   if($_POST['action'] = "Manual Search") {
-    var $fastfood_options = fastfoodOptions();
+    // var $fastfood_options = fastfoodOptions();
 
     var $option_outputs = "";
-    foreach($fastfood_options as $option) {
-      $option_outputs .= '<p>Name: ' . $option['fastfoodName'] . '<br>' . $option['fastfoodLocation'] . '</p>';
-    }
+    // foreach($fastfood_options as $option) {
+    //   $option_outputs .= '<p>Name: ' . $option['fastfoodName'] . '<br>' . $option['fastfoodLocation'] . '</p>';
+    // }
   }
   if($_POST['action'] = "Auto Search") {
-    var $fastfood_options = fastfoodOptions();
+    // var $fastfood_options = fastfoodOptions();
 
     var $option_outputs = '<form action="." method="post"><input type="text" value=""></form>';
-    foreach($fastfood_options as $option) {
-      $option_outputs .= '<p>Name: ' . $option['fastfoodName'] . '<br>' . $option['fastfoodLocation'] . '</p>';
-    }
+    // foreach($fastfood_options as $option) {
+    //   $option_outputs .= '<p>Name: ' . $option['fastfoodName'] . '<br>' . $option['fastfoodLocation'] . '</p>';
+    // }
   }
 
   include 'view.php';
+  exit;
