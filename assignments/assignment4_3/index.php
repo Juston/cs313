3bd77cea80a4b08b7d5f280b9acc9022a801cf78
Juston Cheney <juston.cheney@gmail.com>
2014-10-18 20:51:55 -0600

    Undid everything else

--- assignments/assignment4_3/index.php
+++ assignments/assignment4_3/index.php
@@ -3,24 +3,23 @@
 
 //  include library.php';
 //  include 'model.php';
-  echo "crazy";
 
-  if($_POST['action'] = "Manual Search") {
+  // if($_POST['action'] = "Manual Search") {
     // var $fastfood_options = fastfoodOptions();
 
-    var $option_outputs = "";
+    // var $option_outputs = "";
     // foreach($fastfood_options as $option) {
     //   $option_outputs .= '<p>Name: ' . $option['fastfoodName'] . '<br>' . $option['fastfoodLocation'] . '</p>';
     // }
-  }
-  if($_POST['action'] = "Auto Search") {
+  // }
+  // if($_POST['action'] = "Auto Search") {
     // var $fastfood_options = fastfoodOptions();
 
-    var $option_outputs = '<form action="." method="post"><input type="text" value=""></form>';
+    // var $option_outputs = '<form action="." method="post"><input type="text" value=""></form>';
     // foreach($fastfood_options as $option) {
     //   $option_outputs .= '<p>Name: ' . $option['fastfoodName'] . '<br>' . $option['fastfoodLocation'] . '</p>';
     // }
-  }
+  // }
 
   include 'view.php';
   exit;
