b2184d5efbbcb50e0c0105c8fb453bffcf3ec440
Juston Cheney <juston.cheney@gmail.com>
2014-10-21 08:03:39 -0600

    Adding assignment 5-2

--- assignments/assignment5_2/view.php
+++ assignments/assignment5_2/view.php
@@ -7,7 +7,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Assignments | Everything BYU-I</title>
+  <title>Assignment 5-2 | Everything BYU-I</title>
   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
   <link href="/css/styles.css" rel="stylesheet" type="text/css">
   <script src="/js/header.js" type="text/javascript"></script>
@@ -26,10 +26,21 @@
 </header>
   <section class='container'>
     	<div id="front_page_text">
-    		<h2>Assignments</h2>
-    		<p><a href="/assignments/assignment2_3/" title="Assignment 2_3">Assignment 2_3</a></p>
-        <p><a href="/assignments/assignment4_3/" title="Assignment 4_3">Assignment 4_3</a></p>
-        <p><a href="/assignments/assignment5_2/" title="Assignment 4_3">Assignment 5_2</a></p>
+        <!-- Display Scriptures In Database -->
+
+
+        <!-- Show Form -->
+        Add Scripture
+        <form action="" method="post">
+          <label>Book</label>
+            <input type="text" value="">
+          <label>Chapter</label>
+            <input type="text" value="">
+          <label>Verse</label>
+            <input type="text" value="">
+          <label>Content</label>
+            <input type="textarea" value="">
+        </form>
     	</div>
   </section>
 
