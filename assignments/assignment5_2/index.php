a3a9b386e9edbca1e5613d9f29db0fe50cc2a43c
Juston Cheney <juston.cheney@gmail.com>
2014-10-21 21:50:01 -0600

    Testing

--- assignments/assignment5_2/index.php
+++ assignments/assignment5_2/index.php
@@ -9,8 +9,8 @@ if($_POST['action'] == 'Add Scripture') {
 	$check = $_POST['check'];
 
 	if(!empty($book) && !empty($chapter) && !empty($verse) && !empty($content) && !empty($check)) {
-		$insertScripture = insertScripture($book, $chapter, $verse, $content);
-	
+	//	$insertScripture = insertScripture($book, $chapter, $verse, $content);
+	echo "Whoot whoot!";
 	//	if() {
 	//		$insertTopic = insertTopic();
 	// 	}
