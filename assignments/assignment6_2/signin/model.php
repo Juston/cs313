9aa1d3ed0ee71453bf543bb8560c4fbeff5e4842
Juston Cheney <juston.cheney@gmail.com>
2014-10-28 17:20:14 -0600

    i

--- assignments/assignment6_2/signin/model.php
+++ assignments/assignment6_2/signin/model.php
@@ -4,7 +4,7 @@ function checkPassword($username, $password) {
 	$db = connect();
     $sql = 'SELECT userPassword FROM users WHERE userName = :username';
     $stmt = $db->prepare($sql);
-    $stmt->bindValue(':username', $username);
+    $stmt->bindValue(':username', "$username");
     $stmt->execute();
     $hashed = $stmt->fetch();
     $hashedPassword = $hashed['userPassword'];
