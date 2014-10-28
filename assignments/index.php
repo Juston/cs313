6aab3e4dabbdeec1335649879381b038b2d7bd36
Juston Cheney <juston.cheney@gmail.com>
2014-10-28 08:23:18 -0600

    Added new assignment to assignment/index

--- assignments/index.php
+++ assignments/index.php
@@ -29,7 +29,8 @@
     		<h2>Assignments</h2>
     		<p><a href="/assignments/assignment2_3/" title="Assignment 2_3">Assignment 2_3</a></p>
         <p><a href="/assignments/assignment4_3/" title="Assignment 4_3">Assignment 4_3</a></p>
-        <p><a href="/assignments/assignment5_2/" title="Assignment 4_3">Assignment 5_2</a></p>
+        <p><a href="/assignments/assignment5_2/" title="Assignment 5_2">Assignment 5_2</a></p>
+        <p><a href="/assignments/assignment6_2/" title="Assignment 6_2">Assignment 6_2</a></p>
     	</div>
   </section>
 
