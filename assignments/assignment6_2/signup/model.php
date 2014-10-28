463bb585929bcfc5a8c30113002afb048e3bb491
Juston Cheney <juston.cheney@gmail.com>
2014-10-28 14:59:30 -0600

    i

--- assignments/assignment6_2/signup/model.php
+++ assignments/assignment6_2/signup/model.php
@@ -12,6 +12,8 @@ function insertNewUser($userName, $password) {
 	$salt = '$5$rounds=25000$'.substr(md5(uniqid(rand(),true)),0,16).'$';
     $hashedPassword = crypt($password, $salt);
 
+    echo $hashedPassword;
+
 	$db = connect();
 	$sql = "INSERT INTO users (userName, userPassword) VALUES(:userName, :password)";
 	$stmt = $db->prepare($sql);
