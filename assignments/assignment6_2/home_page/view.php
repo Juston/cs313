13aeb591c16d21f60660332265c2651feb19d93c
Juston Cheney <juston.cheney@gmail.com>
2014-10-28 18:02:59 -0600

    i

--- assignments/assignment6_2/home_page/view.php
+++ assignments/assignment6_2/home_page/view.php
@@ -1,12 +1,39 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Home Page!</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Sign Up | Everything BYU-I</title>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+  <link href="/css/styles.css" rel="stylesheet" type="text/css">
+  <script src="/js/header.js" type="text/javascript"></script>
+
 </head>
-<body>
+<body class="front-page">
+<header>
+	<h1>Signing Up | Everything BYU-I</h1>
+	<div id="menu_icon" class="closed" onclick="menu_animation()"></div>
+  <div id="menu">
+    <ul>
+      <li><a href="/" title="">home</a></li>
+      <li><a href="/assignments/" title="">assignments</a></li>
+    </ul>
+  </div>
+</header>
+  <section class='container'>
+    <div id="front_page_text">
+    	<h2>Sign Up</h2>
+      <?php if(!empty($errors)) {
+        foreach($errors as $error) {
+          echo $error;
+        }
+      } ?>
 	Welcome <?php echo $_SESSION['userName']; ?>
 	<form method="post" action=".">
 		<input type="submit" name="action" value="Sign Out">
 	</form>
+	</div>
+  </section>
+
 </body>
 </html>
